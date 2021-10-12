3407b7716ea9998578bcf7d2be42df39268cb8c9
JORGE <jcgarciamed@gmail.com>
2021-10-11 23:34:27 -0500

    transacciones y procedure 2

--- gm-api-rest-1/src/main/java/com/example/apirest1/controller/ConsultaController.java
+++ gm-api-rest-1/src/main/java/com/example/apirest1/controller/ConsultaController.java
@@ -1,9 +1,41 @@
 package com.example.apirest1.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.apirest1.dto.ConsultaListaExamen;
+import com.example.apirest1.dto.ConsultaResumen;
+import com.example.apirest1.entities.Consulta;
+import com.example.apirest1.service.ConsultaService;
+
 @RestController
 @RequestMapping("/consultas")
-public class ConsultaController {	
+public class ConsultaController {
+	
+	@Autowired
+	private ConsultaService consultaService;
+	
+	@PostMapping
+	public ResponseEntity<Consulta> registrar(@RequestBody ConsultaListaExamen consultaExamenes){
+		Consulta consulta = consultaService.registrarTransaccional(consultaExamenes);
+		return new ResponseEntity<Consulta>(consulta,HttpStatus.CREATED);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<ConsultaResumen>> listarResumen(){
+		List<ConsultaResumen> reporte = consultaService.listarResumen();
+		return new ResponseEntity<List<ConsultaResumen>>(reporte,HttpStatus.CREATED);
+	}
+	
+	
+	
+	
 }
